8ebbe83ad0d8304f04950f67e2abefc4d7e9f7be
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-06-02 21:49:55 -0400

    file structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Components/Navbar/navbar';
 import ProductPage from './Components/ProductsPage/products'
 import Footer from './Components/Footer/footer';
+import './App.css'
 
 function App() {
   return (
